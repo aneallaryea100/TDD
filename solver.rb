c614f66a2a51c9c766ab63acf23b7561f583ba79
Aneal Laryea <aneallaryea100@gmail.com>
2022-10-14 20:42:09 +0000

    Add solver file

---
+++ solver.rb
@@ -0,0 +1,27 @@
+class Solver
+  def factorial(number)
+    return 'No negative number accepted' if is_negative?(number)
+    return 1 if number.zero?
+
+    number * factorial(number - 1)
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    if (number % 15).zero?
+      'fizzbuzz'
+    elsif (number % 5).zero?
+      'buzz'
+    elsif (number % 3).zero?
+      'fizz'
+    else
+      number
+    end
+  end
+end
+
+try = Solver.new
+puts try.fizzbuzz(225)
